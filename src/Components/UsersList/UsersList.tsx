8bf1feff40a613942bd11a4ed475bc0d225aab57
sarrah171996 <sarrahmahmoud714@gmail.com>
2024-12-26 21:33:00 +0200

    c1

--- src/Components/UsersList/UsersList.tsx
+++ src/Components/UsersList/UsersList.tsx
@@ -3,79 +3,88 @@ import React, { useEffect, useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 
 export default function UsersList() {
 
-  const [users , setUser] = useState([])
+  const [users, setUser] = useState([])
 
   const [show, setShow] = useState(false);
 
+  const [userDtat, setUserData] = useState()
+  const [userId, setUserId] = useState(0)
+
   const handleClose = () => setShow(false);
 
-  const handleShow = (id) => {
-    alert(id)
+  const handleShow = (user) => {
+    setUserId(user.id)
+    setUserData(user)
     setShow(true)
-    
+
   };
 
-const getUsers = async()=>{
-  try {
-    let response = await axios.get('https://dummyjson.com/users')
-    setUser(response.data.users);
-    
-    
 
-  } catch (error) {
-    console.log(error);
-    
+
+  const getUsers = async () => {
+    try {
+      let response = await axios.get('https://dummyjson.com/users')
+      setUser(response.data.users);
+
+
+
+    } catch (error) {
+      console.log(error);
+
+    }
+
   }
 
-}
+  const deleteUser = async () => {
+    try {
 
-const deleteUser = async(id)=>{
-  try {
-    
-    let response = await axios.delete(`https://dummyjson.com/users/${id}`)
-console.log({users : users});
-    
-    console.log({res : response});
-    
-  } catch (error) {
-    console.log(error);
-    
+      let response = await axios.delete(`https://dummyjson.com/users/${userId}`)
+      console.log({ users: users });
+
+      console.log({ res: response });
+      toast.success('deleted successfully')
+      handleClose()
+
+    } catch (error) {
+      console.log(error);
+
+    }
   }
-}
 
 
-const editUser = async(id)=>{
-  try {
-    
-    let response = await axios.put(`https://dummyjson.com/users/${id}`)
-    console.log({res : response});
- navigateToEditUserData
-    
-  } catch (error) {
-    console.log(error);
-    
+  // const editUser = async(id)=>{
+  //   try {
+
+  //     let response = await axios.put(`https://dummyjson.com/users/${id}`)
+  //     console.log({res : response});
+  //  navigateToEditUserData
+
+  //   } catch (error) {
+  //     console.log(error);
+
+  //   }
+  // }
+
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+
+  let navigate = useNavigate()
+
+  let navigateToUserData = () => {
+    navigate('/dashboard/adduser')
   }
-}
 
-
-useEffect(()=>{
-  getUsers()
-},[])
-
-
-let navigate = useNavigate()
-
-let navigateToUserData = ()=>{
-  navigate('/dashboard/adduser')
-}
-
-let navigateToEditUserData = ()=>{
-  navigate('/dashboard/adduser')
-}
+  let navigateToEditUserData = () => {
+    navigate('/dashboard/adduser')
+  }
 
 
 
@@ -84,61 +93,61 @@ let navigateToEditUserData = ()=>{
 
 
       <Modal show={show} onHide={handleClose}>
-        <Modal.Body>Are you sure you want to delete ?</Modal.Body>
+        <Modal.Body>Are you sure you want to delete {userDtat?.firstName} {userDtat?.lastName} ?</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             No
           </Button>
-          <Button variant="warning" onClick={()=>handleShow} >
+          <Button variant="warning" onClick={deleteUser} >
             Yes
           </Button>
         </Modal.Footer>
       </Modal>
 
 
-    <div className='title p-3 d-flex justify-content-between'>
-      <h3>Users List</h3>
-      <button className='btn btn-warning' onClick={navigateToUserData}>Add New User</button>
-    </div>
-    <hr />
+      <div className='title p-3 d-flex justify-content-between'>
+        <h3>Users List</h3>
+        <button className='btn btn-warning' onClick={navigateToUserData}>Add New User</button>
+      </div>
+      <hr />
 
-<div className="p-3">
+      <div className="p-3">
 
-<table className="table">
-  <thead>
-    <tr>
-      <th scope='col'></th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Phone</th>
-      <th scope="col">Birth Date</th>
-      <th scope='col'></th>
-    </tr>
-  </thead>
-  <tbody>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope='col'></th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Birth Date</th>
+              <th scope='col'></th>
+            </tr>
+          </thead>
+          <tbody>
 
-{users.map((user:any)=> (
-  <tr key={ user.id}>
-  <th scope="row"><img className='w-25' src={user.image} alt="" /></th>
-  <td>{user.firstName} {user.lastName} </td>
-  <td>{user.email}</td>
-  <td>{user.phone}</td>
-  <td>{user.birthDate} </td>
-  <td>
-   <button onClick={()=>(handleShow(user?.id))} className='border-0 bg-transparent'><i className="fa-solid fa-pen text-warning mx-2"></i> </button> 
-   <button onClick={()=>handleShow(user?.id)} className='border-0 bg-transparent'><i className="fa-regular fa-trash-can text-warning mx-2"></i></button> 
-    </td>
-</tr>
-)
-)}
-
-
-  </tbody>
-</table>
+            {users.map((user: any) => (
+              <tr key={user.id}>
+                <th scope="row"><img className='w-25' src={user.image} alt="" /></th>
+                <td>{user.firstName} {user.lastName} </td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>{user.birthDate} </td>
+                <td>
+                  <button onClick={() => (handleShow(user?.id))} className='border-0 bg-transparent'><i className="fa-solid fa-pen text-warning mx-2"></i> </button>
+                  <button onClick={() => handleShow(user)} className='border-0 bg-transparent'><i className="fa-regular fa-trash-can text-warning mx-2"></i></button>
+                </td>
+              </tr>
+            )
+            )}
 
 
+          </tbody>
+        </table>
 
-</div>
+
+
+      </div>
 
 
 
